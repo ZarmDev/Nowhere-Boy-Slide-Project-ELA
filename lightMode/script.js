f44a02967710f252d0118b19bb46d17f34b5f2cf
ZarmDev <80705328+ZarmDev@users.noreply.github.com>
2021-06-01 18:28:53 -0400

    Much needed CSS

--- lightMode/script.js
+++ lightMode/script.js
@@ -1,3 +1,4 @@
+/*
 var user_password = prompt("Hello! Good sunrise, morning, afternoon, sunset, evening, twilight, night, midnight, and witching hour!! Type the password to enter the project...")
 var password = "2+2=5";
 if (user_password === password) {
@@ -6,3 +7,4 @@ if (user_password === password) {
   alert("Sorry, that's incorrect.");
   wrongPassword = 1;
 }
+*/
