851e2e1fb8334a95408a6f16f396c93f202375c7
ZarmDev <80705328+ZarmDev@users.noreply.github.com>
2021-06-09 13:38:03 -0400

    Fixed theme again

--- theme/script.js
+++ theme/script.js
@@ -1,11 +1,8 @@
-var arrayOfQ = [`https://www.goodreads.com/book/show/36717949-nowhere-boy`, `Page 477-478`, `Page 409-410`];
+var arrayOfQ = [`https://www.goodreads.com/book/show/36717949-nowhere-boy`, `Page 477-478`];
 
 var ogAr = [`Nowhere Boy`, `“Even in the darkest times and places there were always good people. People who would help
 others out of the kindness of their hearts.” “Why did you teach me to love,
-then leave me when my heart became attached to you.”`, `“there were no parents or grandparents sitting on the benches,
-no babies wailing from their strollers, no toddlers 
-shouting up the slides or clambering up the mound. Ahmed instantly understood: 
-people were afraid to be out.”`];
+then leave me when my heart became attached to you.”`];
 
 function eArr(input) {
     document.getElementsByTagName('i')[input].innerHTML = arrayOfQ[input];
