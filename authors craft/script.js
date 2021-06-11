cb3a62f5e65d2a2ed69d9ee3a54c4886db283a99
ZarmDev <80705328+ZarmDev@users.noreply.github.com>
2021-06-11 12:47:35 -0400

    Fixed

--- authors craft/script.js
+++ authors craft/script.js
@@ -3,7 +3,10 @@ var arrayOfQ = [`https://www.goodreads.com/book/show/36717949-nowhere-boy`, `Pag
 var ogAr = [`Nowhere Boy`, `he imagined how it must feel for him to hear the world outside—the shouts from the schoolyard,
 the planes overhead, the sirens and honks and construction noises—and know he couldn’t be part of it.
 Whatever Ahmed had seen, whatever he was afraid of being sent back to,
-had to have been horrible for him to prefer a life like this.`, `Ahmed would have to try; he had lost faith in his own.
+had to have been horrible for him to prefer a life like this.`, `With Fontaine charging after them, would they have the time?
+The only other way across was to hold on to the end of the wall and reach a leg across
+the abyss to the ledge of the next roof. If they fell, it would be a direct drop to the ground,
+three stories below.`, `Ahmed would have to try; he had lost faith in his own.
 Would a Jewish organization really want to help a Muslim boy?
 They probably just resented the comparison between his plight and Ralph’s. 
 People were always weighing their suffering against others’,`];
