d2647beb781f1567800c4565f24f184bf3894c5e
ZarmDev <80705328+ZarmDev@users.noreply.github.com>
2021-06-11 12:35:46 -0400

    add for present

--- authors craft/script.js
+++ authors craft/script.js
@@ -1,6 +1,9 @@
-var arrayOfQ = [`https://www.goodreads.com/book/show/36717949-nowhere-boy`, `Page 539-540`];
+var arrayOfQ = [`https://www.goodreads.com/book/show/36717949-nowhere-boy`, `Page 243-244`, `Page 539-540`];
 
-var ogAr = [`Nowhere Boy`, `Ahmed would have to try; he had lost faith in his own.
+var ogAr = [`Nowhere Boy`, `he imagined how it must feel for him to hear the world outside—the shouts from the schoolyard,
+the planes overhead, the sirens and honks and construction noises—and know he couldn’t be part of it.
+Whatever Ahmed had seen, whatever he was afraid of being sent back to,
+had to have been horrible for him to prefer a life like this.`, `Ahmed would have to try; he had lost faith in his own.
 Would a Jewish organization really want to help a Muslim boy?
 They probably just resented the comparison between his plight and Ralph’s. 
 People were always weighing their suffering against others’,`];
